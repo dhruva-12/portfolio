fff2b1e6fb5fc0ab91df4917b0ddcfa2ee2e7fc1
Dhruva <dhruvashah1221999@gmail.com>
2020-08-12 23:34:29 +0530

    Updates

---
+++ precache-manifest.34e6b50c65def5b2d41ed4b20cdbcea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec41d02b8ac434dfdf3ce9cff89bb81",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5a3473ff1f3cb0e12736",
+    "url": "/portfolio/static/css/main.59025df4.chunk.css"
+  },
+  {
+    "revision": "bf49fce1133a8d9f838e",
+    "url": "/portfolio/static/js/2.d108c997.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d108c997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3473ff1f3cb0e12736",
+    "url": "/portfolio/static/js/main.a6f6fa17.chunk.js"
+  },
+  {
+    "revision": "a82b76e3079a616b48b5",
+    "url": "/portfolio/static/js/runtime-main.ab3d5e0f.js"
+  }
+]);
